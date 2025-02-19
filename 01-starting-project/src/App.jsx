85b104696045b3e7549c6d57fa87892589bc0c96
Cody Moskall <hobbies@techvet.dev>
2025-02-19 17:15:34 -0500

    Remove import, Import dynamic props via index and key name to display content

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,58 +1,71 @@
 import imgPicture from "./assets/react-core-concepts.png";
-import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data.js";
+
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
+    return Math.floor(Math.random() * (max + 1));
 }
 
 function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <div>
-      <header>
-        <img src={imgPicture} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-    </div>
-  );
+    const description = reactDescriptions[genRandomInt(2)];
+    return (
+        <div>
+            <header>
+                <img src={imgPicture} alt="Stylized atom" />
+                <h1>React Essentials</h1>
+                <p>
+                    {description} React concepts you will need for almost any
+                    app you are going to build!
+                </p>
+            </header>
+        </div>
+    );
 }
 
 function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.img} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
+    return (
+        <li>
+            <img src={props.img} alt={props.title} />
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+        </li>
+    );
 }
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            <CoreConcept
-              title="Components"
-              description="The core UI building block."
-              img={componentsImg}
-            />
-            <CoreConcept title="Props" />
-            <CoreConcept />
-            <CoreConcept />
-          </ul>
-        </section>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept
+                            title={CORE_CONCEPTS[0].title}
+                            description={CORE_CONCEPTS[0].description}
+                            img={CORE_CONCEPTS[0].image}
+                        />
+                        <CoreConcept
+                            title={CORE_CONCEPTS[1].title}
+                            description={CORE_CONCEPTS[1].description}
+                            img={CORE_CONCEPTS[1].image}
+                        />
+                        <CoreConcept
+                            title={CORE_CONCEPTS[2].title}
+                            description={CORE_CONCEPTS[2].description}
+                            img={CORE_CONCEPTS[2].image}
+                        />
+                        <CoreConcept
+                            title={CORE_CONCEPTS[3].title}
+                            description={CORE_CONCEPTS[3].description}
+                            img={CORE_CONCEPTS[3].image}
+                        />
+                    </ul>
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
